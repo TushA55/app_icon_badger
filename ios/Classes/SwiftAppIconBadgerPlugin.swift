3799c5e572df6c45c2324bd8ea1ec18e87b95c57
sundarnivash <sundar.j@xmplar.in>
2022-07-13 18:43:06 +0530

    IOS code added

--- ios/Classes/SwiftAppIconBadgerPlugin.swift
+++ ios/Classes/SwiftAppIconBadgerPlugin.swift
@@ -2,13 +2,57 @@ import Flutter
 import UIKit
 
 public class SwiftAppIconBadgerPlugin: NSObject, FlutterPlugin {
+ 
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "app_icon_badger", binaryMessenger: registrar.messenger())
     let instance = SwiftAppIconBadgerPlugin()
+   
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+      var application = UIApplication.shared
+    switch call.method {
+        case NotificationConstants.CHANNEL_METHOD_GET_BADGE_COUNT:
+            let totalCount = getNotificationCount();
+        result(totalCount)
+        case NotificationConstants.CHANNEL_METHOD_SET_BADGE_COUNT:
+            let nCount:Int = call.arguments as! Int
+            let totalCount = setNotificationCount(count: nCount)
+            application.applicationIconBadgeNumber = totalCount
+        result(totalCount)
+        case NotificationConstants.CHANNEL_METHOD_RESET_BADGE:
+            let totalCount = setNotificationCount(count: 0)
+            application.applicationIconBadgeNumber = totalCount
+        result(0)
+        case NotificationConstants.CHANNEL_METHOD_INCREMENT_BADGE_COUNT:
+            let totalCount : Int = getNotificationCount() + 1;
+            application.applicationIconBadgeNumber = setNotificationCount(count: totalCount)
+        result(totalCount)
+        case NotificationConstants.CHANNEL_METHOD_DECREMENT_BADGE_COUNT:
+            let totalCount:Int = getNotificationCount() - 1;
+            application.applicationIconBadgeNumber = setNotificationCount(count: totalCount)
+        result(totalCount)
+        default:
+        result(FlutterMethodNotImplemented)
   }
+
+  
+}
+func getNotificationCount() -> Int{
+    var notificationCount : Int = 0
+    let preferences = UserDefaults.standard
+    let notifyPrefs = "notifyPrefs"
+    if preferences.object(forKey: notifyPrefs) == nil {
+    } else {
+        notificationCount = preferences.integer(forKey: notifyPrefs)
+    }
+    return notificationCount
+}
+func setNotificationCount(count:Int)->Int{
+    let preferences = UserDefaults.standard
+    let currentLevelKey = "notifyPrefs"
+    preferences.set(count, forKey: currentLevelKey)
+    return count
+}
 }
---
+++ ios/Classes/constants.swift
@@ -0,0 +1,14 @@
+import Foundation
+struct NotificationConstants {
+static let CHANNEL_FLUTTER_PLUGIN = "app_icon_badger"
+
+static let BADGE_COUNT = "badgeCount"
+
+static let CHANNEL_METHOD_BADGE_SUPPORTED = "badgeSupported"
+static let CHANNEL_METHOD_GET_BADGE_COUNT = "getBadgeCount"
+static let CHANNEL_METHOD_SET_BADGE_COUNT = "setBadgeCount"
+static let CHANNEL_METHOD_INCREMENT_BADGE_COUNT = "incBadgeCount"
+static let CHANNEL_METHOD_DECREMENT_BADGE_COUNT = "decBadgeCount"
+static let CHANNEL_METHOD_RESET_BADGE = "resetBadge"
+    
+}
